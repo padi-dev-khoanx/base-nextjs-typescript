278254ba3c6aa3b80d2485c8655f67c37620aae9
padi-dev-hoantm <hoantm@paditech.com>
2023-03-03 13:35:21 +0700

    tmp

--- src/components/seller/event/EventSellerItem.tsx
+++ src/components/seller/event/EventSellerItem.tsx
@@ -1,25 +1,14 @@
-import { UserGroup } from "@/src/type/artist.type";
-import { routerConstant } from "@/src/utils/constant/routerConstant";
-import Link from "next/link";
+import { UserGroup } from '@/src/type/artist.type';
+import { routerConstant } from '@/src/constant/routerConstant';
+import Link from 'next/link';
 
-export const EventSellerItem = ({
-  data,
-  id,
-}: {
-  data: UserGroup;
-  id: string;
-}) => {
+export const EventSellerItem = ({ data, id }: { data: UserGroup; id: string }) => {
   return (
-    <div className="flex justify-between items-center px-[24px] py-[31px] border-b border-b-[#D9D9D9] ml-1">
-      <div className="flex-1">
-        <strong className="text-base">{data.name}</strong>
+    <div className='flex justify-between items-center px-[24px] py-[31px] border-b border-b-[#D9D9D9] ml-1'>
+      <div className='flex-1'>
+        <strong className='text-base'>{data.name}</strong>
       </div>
-      <Link
-        href={`${routerConstant.seller.manage_artist.event.edit(
-          id,
-          String(data.id)
-        )}`}
-      >
+      <Link href={`${routerConstant.seller.manage_artist.event.edit(id, String(data.id))}`}>
         変更
       </Link>
     </div>
