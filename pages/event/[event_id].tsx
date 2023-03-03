0563e62704fc88291fb827a300a858f7d76544f5
padi-dev-khoanx <khoanx@paditech.com>
2023-03-03 16:04:33 +0700

    Update

--- pages/event/[event_id].tsx
+++ pages/event/[event_id].tsx
@@ -1,12 +1,12 @@
-import Footer from "@/src/components/common/Footer";
-import Header from "@/src/components/common/Header";
-import LayoutDocument from "@/src/components/layouts/LayoutDocument";
-import EventDetailView from "@/src/view/user/event/EventDetailView";
-import React from "react";
+import Footer from '@/src/components/common/Footer';
+import Header from '@/src/components/common/Header';
+import LayoutDocument from '@/src/components/layouts/LayoutDocument';
+import EventDetailView from '@/src/view/event/EventDetailView';
+import React from 'react';
 
 const EventDetail = () => {
   return (
-    <LayoutDocument title="Event Detail">
+    <LayoutDocument title='Event Detail'>
       <Header />
       <EventDetailView />
       <Footer />
--- pages/event/index.tsx
+++ pages/event/index.tsx
@@ -1,12 +1,12 @@
-import Footer from "@/src/components/common/Footer";
-import Header from "@/src/components/common/Header";
-import LayoutDocument from "@/src/components/layouts/LayoutDocument";
-import EventListView from "@/src/view/user/event/EventListView";
-import React from "react";
+import Footer from '@/src/components/common/Footer';
+import Header from '@/src/components/common/Header';
+import LayoutDocument from '@/src/components/layouts/LayoutDocument';
+import EventListView from '@/src/view/event/EventListView';
+import React from 'react';
 
 const Event = () => {
   return (
-    <LayoutDocument title="List Event">
+    <LayoutDocument title='List Event'>
       <Header />
       <EventListView />
       <Footer />
